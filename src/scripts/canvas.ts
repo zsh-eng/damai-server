af6903c0bb5de01941b4ae80be69000db2421a28
zsheng <chengzhisheng2001@gmail.com>
2024-08-15 22:33:45 +0800

    feat: add script for saving files

---
+++ src/scripts/canvas.ts
@@ -0,0 +1,15 @@
+import { CanvasClient } from "@/services/canvas/client";
+import { downloadFilesForCourse } from "@/services/canvas/download";
+
+const client = new CanvasClient(process.env.CANVAS_TOKEN!);
+
+const BASE_PATH = "src/scripts/files";
+async function main() {
+    const courses = await client.getAllCourses();
+    console.log(courses);
+    for (const course of courses) {
+        await downloadFilesForCourse(course, BASE_PATH);
+    }
+}
+
+main();
