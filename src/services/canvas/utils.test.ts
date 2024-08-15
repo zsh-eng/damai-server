c7cefcc1b01652408012ead24617889e215eb33a
zsheng <chengzhisheng2001@gmail.com>
2024-08-15 20:01:46 +0800

    feat: helper for converting folder array to graph
    An adjacency list representation is useful for traversal.

--- src/services/canvas/utils.test.ts
+++ src/services/canvas/utils.test.ts
@@ -1,32 +1,123 @@
-import { parseLinkHeaders } from '@/services/canvas/utils';
-import { describe, expect, it } from 'vitest';
+import { CanvasFolder } from "@/services/canvas/schema";
+import {
+    folderArrayToAdjacencyList,
+    parseLinkHeaders,
+} from "@/services/canvas/utils";
+import { describe, expect, it } from "vitest";
 
 const SAMPLE_CANVAS_LINK_HEADER = `<https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10>; rel="current",<https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10>; rel="first",<https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10>; rel="last"`;
 const CORRUPTED_CANVAS_LINK_HEADER = `<https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10>; rel="current",<https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10>; rel="first",<https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10> rel="last"`;
 
-describe('parseLinkHeaders', () => {
-  it('should return empty object if linkHeader is undefined', () => {
-    const linkHeader = undefined;
-    const result = parseLinkHeaders(linkHeader);
-    expect(result).toEqual({});
-  });
-
-  it('should return empty object if linkHeader is empty', () => {
-    const linkHeader = '';
-    const result = parseLinkHeaders(linkHeader);
-    expect(result).toEqual({});
-  })
-
-  it('should return object with links', () => {
-    const result = parseLinkHeaders(SAMPLE_CANVAS_LINK_HEADER);
-    expect(result).toEqual({
-      current: 'https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10',
-      first: 'https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10',
-      last: 'https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10',
+describe("parseLinkHeaders", () => {
+    it("should return empty object if linkHeader is undefined", () => {
+        const linkHeader = undefined;
+        const result = parseLinkHeaders(linkHeader);
+        expect(result).toEqual({});
     });
-  })
 
-  it('should throw error if linkHeader is corrupted', () => {
-    expect(() => parseLinkHeaders(CORRUPTED_CANVAS_LINK_HEADER)).toThrowError('Section could not be split on ";"');
-  })
+    it("should return empty object if linkHeader is empty", () => {
+        const linkHeader = "";
+        const result = parseLinkHeaders(linkHeader);
+        expect(result).toEqual({});
+    });
+
+    it("should return object with links", () => {
+        const result = parseLinkHeaders(SAMPLE_CANVAS_LINK_HEADER);
+        expect(result).toEqual({
+            current:
+                "https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10",
+            first: "https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10",
+            last: "https://canvas.nus.edu.sg/api/v1/courses/63977/folders?page=1&per_page=10",
+        });
+    });
+
+    it("should throw error if linkHeader is corrupted", () => {
+        expect(() =>
+            parseLinkHeaders(CORRUPTED_CANVAS_LINK_HEADER)
+        ).toThrowError('Section could not be split on ";"');
+    });
+});
+
+describe("folderArrayToAdjacencyList", () => {
+    const mockFolder = {
+        id: 1,
+        parent_folder_id: null,
+    } as CanvasFolder;
+    const mockFolder2 = {
+        id: 2,
+        parent_folder_id: 1,
+    } as CanvasFolder;
+    const mockFolder3 = {
+        id: 3,
+        parent_folder_id: 2,
+    } as CanvasFolder;
+    const mockFolder4 = {
+        id: 4,
+        parent_folder_id: 1,
+    } as CanvasFolder;
+
+    it("should return the root folder if given 1 folder", () => {
+        expect(folderArrayToAdjacencyList([mockFolder])).toEqual({
+            root: mockFolder,
+            list: {
+                1: {
+                    folder: mockFolder,
+                    children: [],
+                },
+            },
+        });
+    });
+
+    it("should throw an error if no root folder is found", () => {
+        expect(() => folderArrayToAdjacencyList([mockFolder2])).toThrowError(
+            "No root folder found"
+        );
+    });
+
+    it("should return the root folder and the adjacency list", () => {
+        const result = folderArrayToAdjacencyList([
+            mockFolder,
+            mockFolder2,
+            mockFolder3,
+            mockFolder4,
+        ]);
+        expect(result).toEqual({
+            root: mockFolder,
+            list: {
+                1: {
+                    folder: mockFolder,
+                    children: [mockFolder2, mockFolder4],
+                },
+                2: {
+                    folder: mockFolder2,
+                    children: [mockFolder3],
+                },
+                3: {
+                    folder: mockFolder3,
+                    children: [],
+                },
+                4: {
+                    folder: mockFolder4,
+                    children: [],
+                },
+            },
+        });
+    });
+
+    it("should throw an error if parent folder is not found", () => {
+        const mockFolder5 = {
+            id: 5,
+            parent_folder_id: 6,
+        } as CanvasFolder;
+
+        expect(() =>
+            folderArrayToAdjacencyList([
+                mockFolder,
+                mockFolder2,
+                mockFolder3,
+                mockFolder4,
+                mockFolder5,
+            ])
+        ).toThrowError("Parent folder with ID 6 not found");
+    });
 });
--- src/services/canvas/utils.ts
+++ src/services/canvas/utils.ts
@@ -1,7 +1,8 @@
+import { CanvasFolder } from "@/services/canvas/schema";
 
 /**
  * Parses the link header into a record of links.
- * 
+ *
  * @example
  * ```ts
  * const link = parseLinkHeaders('<https://canvas.instructure.com/api/v1/courses/1/assignments?page=1&per_page=10>; rel="current", <https://canvas.instructure.com/api/v1/courses/1/assignments?page=2&per_page=10>; rel="next", <https://canvas.instructure.com/api/v1/courses/1/assignments?page=1&per_page=10>; rel="first", <https://canvas.instructure.com/api/v1/courses/1/assignments?page=2&per_page=10>; rel="last"')
@@ -9,25 +10,78 @@
  */
 export function parseLinkHeaders(linkHeader?: string): Record<string, string> {
     if (!linkHeader) {
-        return {}
+        return {};
     }
 
-    const parts = linkHeader.split(',')
+    const parts = linkHeader.split(",");
     if (parts.length === 0) {
-        return {}
+        return {};
     }
 
-    const links: Record<string, string> = {}
+    const links: Record<string, string> = {};
     for (const part of parts) {
-        const section = part.split(';')
+        const section = part.split(";");
         if (section.length !== 2) {
-            throw new Error('Section could not be split on ";"')
+            throw new Error('Section could not be split on ";"');
         }
-        const url = section[0].replace(/<(.*)>/, '$1').trim();
-        const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+        const url = section[0].replace(/<(.*)>/, "$1").trim();
+        const name = section[1].replace(/rel="(.*)"/, "$1").trim();
         links[name] = url;
-
     }
 
-    return links 
+    return links;
+}
+
+/**
+ * Returns an adajacency list representing the parent to child relationship of the folders.
+ * @param folders a list of Canvas folders
+ * @returns an object with the root folder and the adjacency list
+ */
+export function folderArrayToAdjacencyList(folders: CanvasFolder[]): {
+    root: CanvasFolder;
+    list: Record<
+        number,
+        {
+            folder: CanvasFolder;
+            children: CanvasFolder[];
+        }
+    >;
+} {
+    const root = folders.find((folder) => !folder.parent_folder_id);
+    if (!root) {
+        throw new Error("No root folder found");
+    }
+
+    const adjacencyList: Record<
+        number,
+        {
+            folder: CanvasFolder;
+            children: CanvasFolder[];
+        }
+    > = {};
+    for (const folder of folders) {
+        adjacencyList[folder.id] = {
+            folder,
+            children: [],
+        };
+    }
+
+    for (const folder of folders) {
+        if (!folder.parent_folder_id) {
+            continue;
+        }
+
+        const parentId = folder.parent_folder_id;
+        const entry = adjacencyList[parentId];
+        if (!entry) {
+            throw new Error(`Parent folder with ID ${parentId} not found`);
+        }
+
+        entry.children.push(folder);
+    }
+
+    return {
+        root,
+        list: adjacencyList,
+    };
 }
